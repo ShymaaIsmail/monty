e9342a61086a1259a862c6c5a5eb1c317463d1c5
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-15 17:13:34 +0300

    read file

--- monty.h
+++ monty.h
@@ -49,17 +49,6 @@ typedef struct instruction_s
 
 } instruction_t;
 /**stack operations*/
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
 /**helpers*/
 char *int_to_string(int number);
 int string_to_integer(char *s);
