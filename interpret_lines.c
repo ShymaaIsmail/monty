777cef68c935c0a72206ddd45179c48c80ddf9c9
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-16 20:36:09 +0300

    atoi fix

--- interpret_lines.c
+++ interpret_lines.c
@@ -2,28 +2,30 @@
 shared_t shared_value = {0, 0};
 
 /**
-* handle_push_param - handle_push_param
-* @line: line
-* @line_number: line_number
-* Return: exit code
+ * handle_push_param - handle_push_param
+ * @line: line
+ * @line_number: line_number
+ * Return: exit code
 */
 int handle_push_param(char **line, unsigned int line_number)
 {
-	int value;
-
+	int *value;
 	int exit_code = EXIT_SUCCESS;
 
 	if (line != NULL && line[1] != NULL)
 	{
-		value = atoi(line[1]);
-		if (*line[1] == '\0' || isspace(*line[1]))
+		value = string_to_integer(line[1]);
+		if (value == NULL || *line[1] == ' '
+			|| *line[1] == '\0' || isspace(*line[1]))
 		{
 			print_monty_error(INVALID_INT, 1, (int)line_number);
 			exit_code = EXIT_FAILURE;
+			free(value);
 		}
 		else
 		{
-			shared_value.n = value;
+			shared_value.n = *value;
+			free(value);
 		}
 	}
 	else
@@ -34,20 +36,17 @@ int handle_push_param(char **line, unsigned int line_number)
 	return (exit_code);
 }
 /**
-* process_line - process_line
-* @stack: stack
-* @line: line
-* @line_number: line_number
-* Return: int
+ * process_line - process_line
+ * @stack: stack
+ * @line: line
+ * @line_number: line_number
+ * Return: int
 */
 int process_line(stack_t **stack, char **line, unsigned int line_number)
 {
 	int exit_code = EXIT_SUCCESS;
-
 	int opcode_index = 0, is_handeled_opcode = 0;
-
 	char *opcode;
-
 	instruction_t instructions[] = { {"push", push},
 	{"pall", pall}, {"pint", pint}, {"pop", pop},
 	{"nop", nop}, {"swap", swap}, {NULL, NULL}
